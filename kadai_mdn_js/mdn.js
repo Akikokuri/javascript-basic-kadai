49464d6469ce79cb2bc41485a674f6d870c8077f
Akikokuri <akikokurikuri@gmail.com>
2024-08-13 12:17:38 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,9 @@
+
+const today=new Date();
+
+const todayYear=(today.getFullYear());
+const todayMonth =(today.getMonth()+1);
+const todayDate =(today.getDate());
+
+
+console.log(todayYear+"年", todayMonth+"月", todayDate+"日");
